a88fa7424b4221f11ffd4521b083f875ce70b9b8
olizilla <oli@zilla.org.uk>
2013-03-15 10:25:18 +0000

    Step 1, Do a README, write an example

---
+++ asciify.js
@@ -0,0 +1,11 @@
+/*
+ * asciify
+ * https://github.com/olizilla/asciify
+ */
+
+module.exports = function (text, opts) {
+
+	if (!opts) opts = { font: 'graffiti' };
+
+	return text;
+};
---
+++ example.js
@@ -0,0 +1,4 @@
+
+var asciify = require('./asciify');
+
+console.log(asciify('Hello!'));

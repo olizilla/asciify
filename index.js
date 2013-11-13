e72988d746ce0d6f0b68507cf15eeb0de950381f
Joacim Löwgren <joacim.lowgren@jayway.com>
2013-11-13 13:45:03 +0100

    OCD changes
    Trailing whitespace and missing semicolons makes kitty sad.

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ module.exports = function (text, opts, callback) {
 	// Ensure text is a string
 	text = text + '';
 
-	if (typeof opts === 'function') { 
-		callback = opts; 
-		opts = null; 
+	if (typeof opts === 'function') {
+		callback = opts;
+		opts = null;
 	}
 
 	// Fix up the opts. Default font is graffiti.
@@ -35,23 +35,23 @@ module.exports = function (text, opts, callback) {
 	opts.font = opts.font || 'graffiti';
 
 	if (typeof opts.font !== 'string') {
-		callback('opts.font should be a font name string')
+		callback('opts.font should be a font name string');
 	}
 
 	// Current implementation depends on figlet-js.
 	figlet.Figlet.write(text, opts.font, function (err, asciifiedText) {
-		if (opts.maxWidth){
+		if (opts.maxWidth) {
 			asciifiedText = trimToMaxWidth(opts.maxWidth, asciifiedText);
 		}
 		callback(err, asciifiedText);
-	});	
+	});
 };
 
 // truncate the ascii art to fit a thing
 function trimToMaxWidth (width, text) {
 	var truncated = text.split('\n').map(function (line) {
 		return line.substring(0, width);
-	})
+	});
 	return truncated.join('\n');
 }
 
@@ -69,7 +69,7 @@ module.exports.getFonts = function (callback) {
 		
 		var fonts = files.map(function (file) {
 			return file.slice(0, file.length - 4); // chop off the '.flf' extension
-		});		
+		});
 
 		callback(err, fonts);
 	});
